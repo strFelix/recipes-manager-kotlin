f68dc893ec43818e321490829e8d712383475330
Lucas Felix <Lucas.felixandrade@hotmail.com.br>
2026-02-25 22:09:34 -0300

    feat: add login screen and missing user image for sign up screen

--- app/src/main/java/br/com/strfelix/recipes_manager_kotlin/screens/LoginScreen.kt
+++ app/src/main/java/br/com/strfelix/recipes_manager_kotlin/screens/LoginScreen.kt
@@ -1,38 +1,34 @@
 package br.com.strfelix.recipes_manager_kotlin.screens
 
 import android.content.res.Configuration
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.RemoveRedEye
-import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
@@ -41,12 +37,12 @@ import androidx.compose.ui.unit.dp
 import br.com.strfelix.recipes_manager_kotlin.R
 import br.com.strfelix.recipes_manager_kotlin.ui.theme.RecipesmanagerkotlinTheme
 
-
 @Composable
-fun SignupScreen(modifier: Modifier = Modifier) {
+fun LoginScreen(modifier: Modifier = Modifier) {
     Box(
         modifier = Modifier
-            .fillMaxSize().background(MaterialTheme.colorScheme.background)
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
     ){
         TopEndCard(modifier = Modifier.align(Alignment.TopEnd))
         BottomStartCard(modifier = Modifier.align(Alignment.BottomStart))
@@ -58,95 +54,20 @@ fun SignupScreen(modifier: Modifier = Modifier) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            TitleComponent()
-            Spacer(modifier = Modifier.height(48.dp))
-            UserImage()
-            SignupUserForm()
+            LoginTitle()
+            LoginForm()
         }
 
     }
 }
 
 @Composable
-fun TitleComponent() {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = stringResource(R.string.sign_up),
-            color = MaterialTheme.colorScheme.primary,
-            style = MaterialTheme.typography.titleLarge
-        )
-        Text(
-            text = stringResource(R.string.create_account),
-            color = MaterialTheme.colorScheme.primary,
-            style = MaterialTheme.typography.titleSmall
-        )
-    }
-}
-
-@Composable
-fun UserImage(modifier: Modifier = Modifier) {
-    Box(
-        modifier = Modifier
-            .size(120.dp)
-    ){
-        Image(
-            painter = painterResource(R.drawable.user),
-            contentDescription = "",
-            modifier = Modifier
-                .size(110.dp)
-                .align(Alignment.Center)
-        )
-        Icon(
-            imageVector = Icons.Default.Edit,
-            tint = MaterialTheme.colorScheme.primary,
-            contentDescription = "",
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-        )
-    }
-}
-
-@Composable
-fun SignupUserForm(modifier: Modifier = Modifier) {
+fun LoginForm(modifier: Modifier = Modifier) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .padding(32.dp)
     ) {
-        OutlinedTextField(
-            value = "",
-            onValueChange = {},
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 4.dp),
-            shape = RoundedCornerShape(16.dp),
-            colors = OutlinedTextFieldDefaults
-                .colors(
-                    focusedBorderColor = MaterialTheme.colorScheme.primary,
-                    unfocusedBorderColor = MaterialTheme.colorScheme.primary
-                ),
-            label = {
-                Text(
-                    text = stringResource(R.string.your_name),
-                    style = MaterialTheme.typography.labelSmall
-                )
-            },
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Default.Person,
-                    contentDescription = "",
-                    tint = MaterialTheme.colorScheme.tertiary
-                )
-            },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Text,
-                capitalization = KeyboardCapitalization.Words,
-                imeAction = ImeAction.Next
-            )
-        )
         OutlinedTextField(
             value = "",
             onValueChange = {},
@@ -167,13 +88,14 @@ fun SignupUserForm(modifier: Modifier = Modifier) {
             },
             leadingIcon = {
                 Icon(
-                    imageVector = Icons.Default.Email,
+                    imageVector = Icons.Default.Person,
                     contentDescription = "",
                     tint = MaterialTheme.colorScheme.tertiary
                 )
             },
             keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Email,
+                keyboardType = KeyboardType.Text,
+                capitalization = KeyboardCapitalization.Words,
                 imeAction = ImeAction.Next
             )
         )
@@ -222,10 +144,52 @@ fun SignupUserForm(modifier: Modifier = Modifier) {
             shape = RoundedCornerShape(8.dp)
         ) {
             Text(
-                text = stringResource(R.string.create_account),
+                text = stringResource(R.string.login_screen_button),
                 style = MaterialTheme.typography.labelMedium
             )
         }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Absolute.Right
+        ) {
+            Text(
+                text = stringResource(R.string.create_account_login_screen),
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            TextButton(
+                onClick = {}
+            ) {
+                Text(
+                    text = stringResource(R.string.sign_up),
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun LoginTitle(){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start,
+        modifier = Modifier.fillMaxWidth()
+            .padding(32.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.login_title),
+            color = MaterialTheme.colorScheme.primary,
+            style = MaterialTheme.typography.titleLarge
+        )
+        Text(
+            text = stringResource(R.string.login_subtitle),
+            color = MaterialTheme.colorScheme.primary,
+            style = MaterialTheme.typography.titleSmall
+        )
     }
 }
 
@@ -237,7 +201,7 @@ fun SignupUserForm(modifier: Modifier = Modifier) {
 @Composable
 private fun SignupUserFormPreview() {
     RecipesmanagerkotlinTheme {
-        SignupUserForm()
+        LoginForm()
     }
 }
 
@@ -247,9 +211,9 @@ private fun SignupUserFormPreview() {
     locale = "en"
 )
 @Composable
-private fun UserImagePreview() {
+private fun LoginTitlePreview() {
     RecipesmanagerkotlinTheme {
-        UserImage()
+        LoginTitle()
     }
 }
 
@@ -258,23 +222,9 @@ private fun UserImagePreview() {
     uiMode = Configuration.UI_MODE_NIGHT_NO,
     locale = "en"
 )
-@Preview
 @Composable
-private fun TitleComponentPreview() {
+private fun LoginScreenPreview() {
     RecipesmanagerkotlinTheme {
-        TitleComponent()
+        LoginScreen()
     }
-
-}
-@Preview(
-    showBackground = true,
-    uiMode = Configuration.UI_MODE_NIGHT_NO,
-    locale = "en"
-)
-@Composable
-private fun SignupScreenPreview() {
-    RecipesmanagerkotlinTheme {
-        SignupScreen()
-    }
-
 }
